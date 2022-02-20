f9ecbb0a7a84c2981a8f6fd05af7497611ef3e73
KonlawatPach <pach.wongsrithong@Gmail.com>
2022-02-21 07:08:26 +0800

    final addYLO

--- js/addYLO.js
+++ js/addYLO.js
@@ -3,18 +3,30 @@ var lastYear = 2;
 function addYear(){
     lastYear++
     $(`
-        <div class="bg-light mt-1 p-3 rounded-2" id="`+lastYear+`">
-            <h5 class="ms-1 fw-bold">ชั้นปีที่ `+ lastYear +`</h5>
-            <div class="row mt-3 ms-4" id="`+ lastYear +`-1">
-                <div class="col-1 p-0 text-center">YLO `+ lastYear +`-1</div>
+        <div class="bg-light mt-3 p-3 rounded-2" id="`+lastYear+`">
+            <h5 class="ms-1 fw-bold">ชั้นปีที่ `+lastYear+`</h5>
+            <div class="row mt-3 ms-4 ylobody`+lastYear+`" id="`+lastYear+`-1">
+                <div class="col-1 p-0 text-center ylo`+lastYear+`">YLO `+lastYear+`-1</div>
                 <div class="col-9 p-0">
-                    <input class="w-100" type="text" id="ylo-`+ lastYear +`-1">
+                    <input class="w-100 yloinput`+lastYear+`" type="text" id="ylo-`+lastYear+`-1">
                 </div>
                 <div class="col-1 p-0 text-center">
-                    <button class="btn-success rounded-3" onclick="addYLO('`+ lastYear +`-1')">เพิ่ม YLO</button>
+                    <button class="btn btn-success btn-sm rounded-3 addylobtn`+lastYear+`" onclick="addYLO('`+lastYear+`-1')">เพิ่ม YLO</button>
                 </div>
                 <div class="col-1 p-0 text-center">
-                    <button class="btn-danger rounded-3" onclick="delYLO('`+ lastYear +`-1')">ลบ YLO</button>
+                    <button class="btn btn-danger btn-sm rounded-3 delylobtn`+lastYear+`" onclick="delYLO('`+lastYear+`-1')">ลบ YLO</button>
+                </div>
+            </div>
+            <div class="row mt-3 ms-4 ylobody`+lastYear+`" id="`+lastYear+`-2">
+                <div class="col-1 p-0 text-center ylo`+lastYear+`">YLO `+lastYear+`-2</div>
+                <div class="col-9 p-0">
+                    <input class="w-100 yloinput`+lastYear+`" type="text" id="ylo-`+lastYear+`-2">
+                </div>
+                <div class="col-1 p-0 text-center">
+                    <button class="btn btn-success btn-sm rounded-3 addylobtn`+lastYear+`" onclick="addYLO('`+lastYear+`-2')">เพิ่ม YLO</button>
+                </div>
+                <div class="col-1 p-0 text-center">
+                    <button class="btn btn-danger btn-sm rounded-3 delylobtn`+lastYear+`" onclick="delYLO('`+lastYear+`-2')">ลบ YLO</button>
                 </div>
             </div>
         </div>
@@ -31,20 +43,20 @@ function deleteYear(){
 
 function addYLO(yloID){
     yloID = yloID.split('-')
-    $(`
-        <div class="row mt-3 ms-4" id="`+yloID[0] +`-`+(Number(yloID[1])+1)+`">
-            <div class="col-1 p-0 text-center">YLO `+ yloID[0] +`-`+ (Number(yloID[1])+1) +`</div>
-            <div class="col-9 p-0">
-                <input class="w-100" type="text" id="ylo-`+ yloID[0] +`-`+ (Number(yloID[1])+1) +`">
-            </div>
-            <div class="col-1 p-0 text-center">
-                <button class="btn-success rounded-3" onclick="addYLO('`+ yloID[0] +`-`+ (Number(yloID[1])+1) +`')">เพิ่ม YLO</button>
-            </div>
-            <div class="col-1 p-0 text-center">
-                <button class="btn-danger rounded-3" onclick="delYLO('`+ yloID[0] +`-`+ (Number(yloID[1])+1) +`')">ลบ YLO</button>
-            </div>
+    $("#"+ yloID[0] +`-`+ (Number(yloID[1]))).after(`
+    <div class="row mt-3 ms-4 ylobody`+ yloID[0] +`" id="`+yloID[0] +`-`+(Number(yloID[1])+1)+`">
+        <div class="col-1 p-0 text-center ylo`+ yloID[0] +`">YLO `+ yloID[0] +`-`+ (Number(yloID[1])+1) +`</div>
+        <div class="col-9 p-0">
+            <input class="w-100 yloinput`+ yloID[0] +`" type="text" id="ylo-`+ yloID[0] +`-`+ (Number(yloID[1])+1) +`">
         </div>
-    `).appendTo("#" + yloID[0]);
+        <div class="col-1 p-0 text-center">
+            <button class="btn btn-success btn-sm rounded-3 addylobtn`+ yloID[0] +`" onclick="addYLO('`+ yloID[0] +`-`+ (Number(yloID[1])+1) +`')">เพิ่ม YLO</button>
+        </div>
+        <div class="col-1 p-0 text-center">
+            <button class="btn btn-danger btn-sm rounded-3 delylobtn`+ yloID[0] +`" onclick="delYLO('`+ yloID[0] +`-`+ (Number(yloID[1])+1) +`')">ลบ YLO</button>
+        </div>
+    </div>
+    `);
     sortNumber(yloID[0])
 }
 
@@ -55,5 +67,14 @@ function delYLO(yloID){
 }
 
 function sortNumber(yloID){
-    //
+    let x = document.getElementsByClassName("ylo" + yloID)
+    for (let i = 1; i <= x.length; i++) {
+        document.getElementsByClassName("ylobody" + yloID)[i-1].setAttribute("id", yloID +"-"+ i );
+        document.getElementsByClassName("ylo" + yloID)[i-1].textContent = "YLO "+ yloID +"-"+ i;
+        document.getElementsByClassName("yloinput" + yloID)[i-1].setAttribute("id", yloID +"-"+ i );
+        document.getElementsByClassName("addylobtn" + yloID)[i-1].setAttribute( "onClick", "addYLO('" + yloID[0] + "-" + i + "')" );
+        document.getElementsByClassName("delylobtn" + yloID)[i-1].setAttribute( "onClick", "delYLO('" + yloID[0] + "-" + i + "')" );
+    }
+    if(x.length == 1) document.getElementsByClassName("delylobtn" + yloID)[0].disabled = true;
+    else document.getElementsByClassName("delylobtn" + yloID)[0].disabled = false;
 }
