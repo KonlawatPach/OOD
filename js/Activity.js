c287c61bd4d6db1440561b0b95c80a16f5ea2030
burapayoky <burapav2@gmail.com>
2022-02-28 15:49:31 +0700

    เเก้เเล้วนะเเต่บัคอยู่ๆ

--- js/Activity.js
+++ js/Activity.js
@@ -10,84 +10,82 @@ function deleteYear(){
 function getsPLOs(num){
     
 }
-function addYLO(yloID){
-    yloID = yloID.split('-')
-    
-    $("#"+ yloID[0] +`-`+ (Number(yloID[1]))).after(`
-    <div class="row mt-3 ms-4 ylobody`+ yloID[0] +`" id="`+yloID[0] +`-`+(Number(yloID[1])+1)+`">
-        <div class="col-2 p-0 text-center ylo`+ yloID[0] +`">รายละเอียดกิจกรรมที่ `+ yloID[0] +`-`+ (Number(yloID[1])+1) +`<span class="text-danger"> *</span></div>
+function addYLO(activityID){
+    activityID = activityID.split('-')
+    $("#"+ activityID[0] +`-`+ (Number(activityID[1]))).after(`
+    <div class="row mt-3 ms-4 activitybody`+ activityID[0] +`" id="`+activityID[0] +`-`+(Number(activityID[1])+1)+`">
+        <div class="col-2 p-0 text-center activity`+ activityID[0] +`">รายละเอียดกิจกรรมที่ `+ activityID[0] +`-`+ (Number(activityID[1])+1) +`<span class="text-danger"> *</span></div>
         <div class="col-8 p-0">
-            <textarea class="w-100 yloinput`+ yloID[0] +`" required autocomplete="off" type="text" id="ylo-`+ yloID[0] +`-`+ (Number(yloID[1])+1) +`" onchange="haveSameText()"></textarea>
+            <textarea class="w-100 activityinput`+ activityID[0] +`" required autocomplete="off" type="text" id="activity-`+ activityID[0] +`-`+ (Number(activityID[1])+1) +`" onchange="haveSameText()"></textarea>
         </div>
         <div class="col-1 p-0 text-center">
-            <button type="button" class="btn btn-success btn-sm rounded-3 addylobtn`+ yloID[0] +`" onclick="addYLO('`+ yloID[0] +`-`+ (Number(yloID[1])+1) +`')">เพิ่มกิจกรรม</button>
+            <button type="button" class="btn btn-success btn-sm rounded-3 addactivitybtn`+ activityID[0] +`" onclick="addYLO('`+ activityID[0] +`-`+ (Number(activityID[1])+1) +`')">เพิ่มกิจกรรม</button>
         </div>
         <div class="col-1 p-0 text-center">
-            <button type="button" class="btn btn-danger btn-sm rounded-3 delylobtn`+ yloID[0] +`" onclick="delYLO('`+ yloID[0] +`-`+ (Number(yloID[1])+1) +`')">ลบกิจกรรม</button>
+            <button type="button" class="btn btn-danger btn-sm rounded-3 delactivitybtn`+ activityID[0] +`" onclick="delYLO('`+ activityID[0] +`-`+ (Number(activityID[1])+1) +`')">ลบกิจกรรม</button>
         </div>
     </div>
     `);
-    sortNumber(yloID[0])
+    sortNumber(activityID[0])
 }
-function addActivity(ActivityID){
-    ActivityID = ActivityID.split('-')
-    $("#"+ ActivityID[0] +`-`+ (Number(ActivityID[1]))).after(`
-    <div class="row mt-3 ms-4 Activitybody`+ ActivityID[0] +`" id="`+ActivityID[0] +`-`+(Number(ActivityID[1])+1)+`">
-        <div class="col-2 p-0 text-center Activity`+ ActivityID[0] +`">วิธีการประเมินเกเร `+ ActivityID[0] +`-`+ (Number(ActivityID[1])+1) +`<span class="text-danger"> *</span></div>
+function addAssessment(AssessmentID){
+    AssessmentID = AssessmentID.split('-')
+    $("#"+ AssessmentID[0] +`-`+ (Number(AssessmentID[1]))).after(`
+    <div class="row mt-3 ms-4 Assessmentbody`+ AssessmentID[0] +`" id="`+AssessmentID[0] +`-`+(Number(AssessmentID[1])+1)+`">
+        <div class="col-2 p-0 text-center Assessment`+ AssessmentID[0] +`">วิธีการประเมินเกเร `+ AssessmentID[0] +`-`+ (Number(AssessmentID[1])+1) +`<span class="text-danger"> *</span></div>
         <div class="col-8 p-0">
-            <textarea class="w-100 Activityinput`+ ActivityID[0] +`" required autocomplete="off" type="text" id="Activity-`+ ActivityID[0] +`-`+ (Number(ActivityID[1])+1) +`" onchange="haveSameTexttwo()"></textarea>
+            <textarea class="w-100 Assessmentinput`+ AssessmentID[0] +`" required autocomplete="off" type="text" id="Assessment-`+ AssessmentID[0] +`-`+ (Number(AssessmentID[1])+1) +`" onchange="haveSameTexttwo()"></textarea>
         </div>
         <div class="col-1 p-0 text-center">
-            <button type="button" class="btn btn-success btn-sm rounded-3 addActivitybtn`+ ActivityID[0] +`" onclick="addActivity('`+ ActivityID[0] +`-`+ (Number(ActivityID[1])+1) +`')">เพิ่มกิจกรรม</button>
+            <button type="button" class="btn btn-success btn-sm rounded-3 addAssessmentbtn`+ AssessmentID[0] +`" onclick="addAssessment('`+ AssessmentID[0] +`-`+ (Number(AssessmentID[1])+1) +`')">เพิ่มกิจกรรม</button>
         </div>
         <div class="col-1 p-0 text-center">
-            <button type="button" class="btn btn-danger btn-sm rounded-3 delActivitybtn`+ ActivityID[0] +`" onclick="delActivity('`+ ActivityID[0] +`-`+ (Number(ActivityID[1])+1) +`')">ลบกิจกรรม</button>
+            <button type="button" class="btn btn-danger btn-sm rounded-3 delAssessmentbtn`+ AssessmentID[0] +`" onclick="delAssessment('`+ AssessmentID[0] +`-`+ (Number(AssessmentID[1])+1) +`')">ลบกิจกรรม</button>
         </div>
     </div>
     `);
-    sortNumber(ActivityID[0])
+    sortassesment(AssessmentID[0])
 }
 
-function delYLO(yloID){
-    console.log("ylo");
-    yloID = yloID.split('-')
-    $('#' + yloID[0] + "-" + yloID[1]).remove()
-    sortNumber(yloID[0])
-}
-function delActivity(ActivityID){
-    console.log("ActivityID");
-    ActivityID = ActivityID.split('-')
-    $('#' + ActivityID[0] + "-" + ActivityID[1]).remove()
-    sortNumber(ActivityID[0])
-}
-
-function sortNumber(yloID){
+function delYLO(activityID){
     
-    let x = document.getElementsByClassName("ylo" + yloID)
+    activityID = activityID.split('-')
+    $('#' + activityID[0] + "-" + activityID[1]).remove()
+    sortNumber(activityID[0])
+}
+function delAssessment(AssessmentID){
+    AssessmentID = AssessmentID.split('-')
+    $('#' + AssessmentID[0] + "-" + AssessmentID[1]).remove()
+    sortassesment(AssessmentID[0])
+}
+
+function sortNumber(activityID){
+    let x = document.getElementsByClassName("activity" + activityID)
     for (let i = 1; i <= x.length; i++) {
-        document.getElementsByClassName("ylobody" + yloID)[i-1].setAttribute("id", yloID +"-"+ i );
-        document.getElementsByClassName("ylo" + yloID)[i-1].textContent = "รายละเอียดกิจกรรมที่ "+ i;
-        document.getElementsByClassName("yloinput" + yloID)[i-1].setAttribute("id", yloID +"-"+ i );
-        document.getElementsByClassName("addylobtn" + yloID)[i-1].setAttribute( "onClick", "addYLO('" + yloID[0] + "-" + i + "')" );
-        document.getElementsByClassName("delylobtn" + yloID)[i-1].setAttribute( "onClick", "delYLO('" + yloID[0] + "-" + i + "')" );
+        document.getElementsByClassName("activitybody" + activityID)[i-1].setAttribute("id", activityID +"-"+ i );
+        document.getElementsByClassName("activity" + activityID)[i-1].textContent = "รายละเอียดกิจกรรมที่"+ i;
+        document.getElementsByClassName("activityinput" + activityID)[i-1].setAttribute("id", activityID +"-"+ i );
+        document.getElementsByClassName("addactivitybtn" + activityID)[i-1].setAttribute( "onClick", "addYLO('" + activityID[0] + "-" + i + "')" );
+        document.getElementsByClassName("delactivitybtn" + activityID)[i-1].setAttribute( "onClick", "delYLO('" + activityID[0] + "-" + i + "')" );
     }
-    $(`<span class="text-danger"> *</span>`).appendTo((".ylo" + yloID));
-    if(x.length == 1) document.getElementsByClassName("delylobtn" + yloID)[0].disabled = true;
-    else document.getElementsByClassName("delylobtn" + yloID)[0].disabled = false;
+    $(`<span class="text-danger"> *</span>`).appendTo((".activity" + activityID));
+    if(x.length == 1) document.getElementsByClassName("delactivitybtn" + activityID)[0].disabled = true;
+    else document.getElementsByClassName("delactivitybtn" + activityID)[0].disabled = false;
 }
-function sortNumber(ActivityID){
+
+function sortassesment(AssessmentID){
     
-    let y = document.getElementsByClassName("Activity" + ActivityID)
+    let y = document.getElementsByClassName("Assessment" + AssessmentID)
     for (let p = 1; p <= y.length; p++) {
-        document.getElementsByClassName("Activitybody" + ActivityID)[p-1].setAttribute("id", ActivityID +"-"+ p );
-        document.getElementsByClassName("Activity" + ActivityID)[p-1].textContent = "วิธีการประเมิน "+ p;
-        document.getElementsByClassName("Activityinput" + ActivityID)[p-1].setAttribute("id", ActivityID +"-"+ p );
-        document.getElementsByClassName("addActivitybtn" + ActivityID)[p-1].setAttribute( "onClick", "addActivity('" + ActivityID[0] + "-" + p + "')" );
-        document.getElementsByClassName("delActivitybtn" + ActivityID)[p-1].setAttribute( "onClick", "delActivity('" + ActivityID[0] + "-" + p + "')" );
+        document.getElementsByClassName("Assessmentbody" + AssessmentID)[p-1].setAttribute("id", AssessmentID +"-"+ p );
+        document.getElementsByClassName("Assessment" + AssessmentID)[p-1].textContent = "วิธีการประเมิน "+ p;
+        document.getElementsByClassName("Assessmentinput" + AssessmentID)[p-1].setAttribute("id", AssessmentID +"-"+ p );
+        document.getElementsByClassName("addAssessmentbtn" + AssessmentID)[p-1].setAttribute( "onClick", "addAssessment('" + AssessmentID[0] + "-" + p + "')" );
+        document.getElementsByClassName("delAssessmentbtn" + AssessmentID)[p-1].setAttribute( "onClick", "delAssessment('" + AssessmentID[0] + "-" + p + "')" );
     }
-    $(`<span class="text-danger"> *</span>`).appendTo((".activity" + ActivityID));
-    if(y.length == 1) document.getElementsByClassName("delActivitybtn" + ActivityID)[0].disabled = true;
-    else document.getElementsByClassName("delActivitybtn" + ActivityID)[0].disabled = false;
+    $(`<span class="text-danger"> *</span>`).appendTo((".Assessment" + AssessmentID));
+    if(y.length == 1) document.getElementsByClassName("delAssessmentbtn" + AssessmentID)[0].disabled = true;
+    else document.getElementsByClassName("delAssessmentbtn" + AssessmentID)[0].disabled = false;
 }
 
 // ส่วนของการ validation
@@ -114,20 +112,20 @@ function haveSameText(){
     let comparetext;
     let lastYear=5;
     for (let year = 1; year <= lastYear; year++) {
-        let x = document.getElementsByClassName("ylo" + year)
+        let x = document.getElementsByClassName("activity" + year)
         for (let i = 1; i <= x.length; i++) {
-            choosetext = document.getElementsByClassName("yloinput" + year)[i-1].value;
+            choosetext = document.getElementsByClassName("activityinput" + year)[i-1].value;
 
             //compare loop zone
             if(choosetext != ''){
                 for (let cyear = 1; cyear <= lastYear; cyear++) {
-                    let y = document.getElementsByClassName("ylo" + cyear)
+                    let y = document.getElementsByClassName("activity" + cyear)
                     for (let j = 1; j <= y.length; j++) {
-                        comparetext = document.getElementsByClassName("yloinput" + cyear)[j-1].value;
+                        comparetext = document.getElementsByClassName("activityinput" + cyear)[j-1].value;
                         if(choosetext == comparetext && (year != cyear || i != j)){
-                            document.getElementsByClassName("yloinput" + year)[i-1].style.borderColor = "red";
-                            document.getElementsByClassName("yloinput" + cyear)[j-1].style.borderColor = "red";
-                            document.getElementsByClassName("yloinput" + year)[i-1].scrollIntoView();
+                            document.getElementsByClassName("activityinput" + year)[i-1].style.borderColor = "red";
+                            document.getElementsByClassName("activityinput" + cyear)[j-1].style.borderColor = "red";
+                            document.getElementsByClassName("activityinput" + year)[i-1].scrollIntoView();
                             hasSame = true;
                         }
                     }
@@ -144,20 +142,20 @@ function haveSameTexttwo(){
     let comparetext;
     let lastYear=5;
     for (let year = 1; year <= lastYear; year++) {
-        let y = document.getElementsByClassName("Activity" + year)
+        let y = document.getElementsByClassName("Assessment" + year)
         for (let i = 1; i <= y.length; i++) {
-            choosetext = document.getElementsByClassName("Activityinput" + year)[i-1].value;
+            choosetext = document.getElementsByClassName("Assessmentinput" + year)[i-1].value;
 
             //compare loop zone
             if(choosetext != ''){
                 for (let cyear = 1; cyear <= lastYear; cyear++) {
-                    let y = document.getElementsByClassName("Activity" + cyear)
+                    let y = document.getElementsByClassName("Assessment" + cyear)
                     for (let j = 1; j <= y.length; j++) {
-                        comparetext = document.getElementsByClassName("Activityinput" + cyear)[j-1].value;
+                        comparetext = document.getElementsByClassName("Assessmentinput" + cyear)[j-1].value;
                         if(choosetext == comparetext && (year != cyear || i != j)){
-                            document.getElementsByClassName("Activityinput" + year)[i-1].style.borderColor = "red";
-                            document.getElementsByClassName("Activityinput" + cyear)[j-1].style.borderColor = "red";
-                            document.getElementsByClassName("Activityinput" + year)[i-1].scrollIntoView();
+                            document.getElementsByClassName("Assessmentinput" + year)[i-1].style.borderColor = "red";
+                            document.getElementsByClassName("Assessmentinput" + cyear)[j-1].style.borderColor = "red";
+                            document.getElementsByClassName("Assessmentinput" + year)[i-1].scrollIntoView();
                             hasSame = true;
                         }
                     }
